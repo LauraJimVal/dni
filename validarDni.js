aeddb3c4b20163dc67d7eee263580079ea4af343
Laura Jiménez Valencia <metsys931@hotmail.com>
2023-02-22 11:26:51 +0100

    subida de archivos

---
+++ validarDni.js
@@ -0,0 +1,18 @@
+function letraDNI(){ 
+    var letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E","T"];
+    var dni = document.getElementById("numDNI").value;
+    var letra = document.getElementById("letraDNI").value;
+    letra = letra.toUpperCase();
+
+    if(dni < 1 || dni > 99999999){
+        mensaje= "Debes indicar un número de DNI";
+    }else{
+        var letraCalculada = letras[dni% 23];
+        if(letraCalculada != letra){
+            mensaje= "La letra no coincide con el número de DNI proporcionado";
+        }else{
+            mensaje="La letra y el número coinciden correctamente";
+        }
+    }
+document.getElementById("resultado").innerHTML = mensaje;
+} 
